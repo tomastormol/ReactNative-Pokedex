e786abd5bba6721d473a2dd73c25fe4ca71e1b6b
Tomas <tomas@Tomass-MacBook-Pro.local>
2023-10-09 16:49:36 +0200

    Added ActivityIndicator

--- Home/Home.jsx
+++ Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, View, FlatList, TextInput } from "react-native";
+import { SafeAreaView, View, FlatList, TextInput, ActivityIndicator } from "react-native";
 import Card from "../components/Card";
 import styles from './styles'
 import SearchBar from "../components/SearchBar";
@@ -8,16 +8,30 @@ import { useFetchPokemons } from "../utils/useFetchPokemons";
 
 export default function Home() {
 
-    const { pokemonsList, pokemonsFilttering, setPokemonsList, setIsLoading, isLoading, isError } = useFetchPokemons()
+    const { pokemonsList, pokemonsFilttering, setPokemonsList, isLoading, isError } = useFetchPokemons()
 
     return (
         <View style={{ padding: 10 }}>
-            <SearchBar setPokemonsList={setPokemonsList} pokemonsFilttering={pokemonsFilttering} />
-            <FlatList
-                data={pokemonsList}
-                keyExtractor={(item) => item.name}
-                renderItem={({ item }) => <Card pokemon={item} />}
-            />
+            {isLoading ? (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator size={"large"} color="#0071DC" />
+                </View>
+            ) : (
+                <View>
+                    <SearchBar setPokemonsList={setPokemonsList} pokemonsFilttering={pokemonsFilttering} />
+                    <FlatList
+                        data={pokemonsList}
+                        keyExtractor={(item) => item.name}
+                        renderItem={({ item }) => <Card pokemon={item} />}
+                    />
+                </View>
+            )}
+            {isError && (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text>Error fetching data... Please check your internet connection</Text>
+                </View>
+            )}
+
         </View>
     );
 }
